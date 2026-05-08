3987f36012c50f4fcd760cb50c526dce5d8bfe41
Hossain Forhad <nextemail5508@gmail.com>
2026-05-08 23:35:00 +0600

    Backend Home Slider Setup

---
+++ app/Http/Controllers/Backend/SliderController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Slider;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class SliderController extends Controller
+{
+    public function getSlider()
+    {
+        $slider = Slider::find(1);
+        return view('admin.backend.slider.get_slider', compact('slider'));
+    } // End Method
+
+    public function updateSlider(Request $request){
+        $slider_id = $request->id;
+        $slider = Slider::findOrFail($slider_id);
+        if($request->file('image')){
+            $image =$request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(306,618)->save(public_path('upload/slider/'.$name_gen));
+            $save_url = 'upload/slider/'.$name_gen;
+
+            if(file_exists(public_path($slider->image))){
+                unlink(public_path($slider->image));
+            }
+
+            Slider::findOrFail($slider_id)->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'link' => $request->link,
+                'image' => $save_url,
+            ]);
+        } else {
+            Slider::findOrFail($slider_id)->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'link' => $request->link,
+            ]);
+        }
+        $notification = array(
+            'message' => 'Slider Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    } // End Method
+
+}
